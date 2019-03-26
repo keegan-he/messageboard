f29e06cd19c0fea707df52ff2a818acaf6d44456
keegan-h <keeganslane@gmail.com>
2019-03-25 17:29:38 -0700

    working with model

--- products/models.py
+++ products/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title           = models.TextField()
-    description     = models.TextField()
-    price           = models.TextField()
-    active          = models.TextField()
+    title           = models.CharField(max_length=120) #when you use a charfield you have ot use a max_length attribute.
+    description     = models.TextField(blank=True, null=True)
+    price           = models.DecimalField(decimal_places=2, max_digits=10000)
+    summary         = models.TextField(blank=False, null=False)
+    featured        = models.BooleanField()
