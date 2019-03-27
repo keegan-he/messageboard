2b9a07a4c8645811a3aca22030f640ee5f4b7806
keegan-h <keeganslane@gmail.com>
2019-03-27 14:01:14 -0700

    adding more views

--- message_board/urls.py
+++ message_board/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages import views
+from pages.views import home_view
 
 
 urlpatterns = [
+    path('', home_view, name='home'),
+    path('contact/', home_view),
     path('admin/', admin.site.urls),
-    path('', views.home_view, name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,5 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_view(*args, **kwargs):
-    print('running the home function')
     return HttpResponse("<h1>HELLOOO WORLDDDD!!!!</h1>")
