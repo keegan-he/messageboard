f51b7977f43c0c8974c81998b71f419c0cef6b07
keegan-h <keeganslane@gmail.com>
2019-03-26 20:56:35 -0700

    printing

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_view(*args, **kwargs):
+    print('running the home function')
+    return HttpResponse("<h1>HELLOOO WORLDDDD!!!!</h1>")
