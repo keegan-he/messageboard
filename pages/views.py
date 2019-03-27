b9949a3aa4d831a053b92aafeafc43c4e4184b10
keegan-h <keeganslane@gmail.com>
2019-03-27 14:07:28 -0700

    added about view

--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def home_view(*args, **kwargs):
     return HttpResponse("<h1>HELLOOO WORLDDDD!!!!</h1>")
+
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h1>Contact page!!!!!!!</h1>")
+
+def about_view(*args, **kwargs):
+    return HttpResponse("<h1>Here is the about page!</h1>")
