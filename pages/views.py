ee24b06bc2cbe5e2ebcd662fe7f4dbb27219c303
keegan-h <keeganslane@gmail.com>
2019-03-27 14:25:26 -0700

    created templates folder

--- pages/views.py
+++ pages/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>HELLOOO WORLDDDD!!!!</h1>")
+def home_view(request, *args, **kwargs):
+    return render(request, 'home.html', {})
 
-def contact_view(*args, **kwargs):
+def contact_view(request, *args, **kwargs):
     return HttpResponse("<h1>Contact page!!!!!!!</h1>")
 
-def about_view(*args, **kwargs):
+def about_view(request, *args, **kwargs):
     return HttpResponse("<h1>Here is the about page!</h1>")
